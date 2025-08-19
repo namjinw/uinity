f5424f5666c2c1d45ee6eb28bc3a5c8bf0ff08c8
namjinw <0812jinwoo@gmail.com>
2025-08-20 01:24:20 +0900

    Add files via upload

--- player.cs
+++ player.cs
@@ -1,32 +1,39 @@
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 3f;
     public float rotationSpeed = 10f;
+
+    private Animator animator;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float moveX = 0f;
         float moveZ = 0f;
 
-        if (Input.GetKey(KeyCode.W)) { moveZ += 1f; }
-        if (Input.GetKey(KeyCode.S)) { moveZ -= 1f; }
-        if (Input.GetKey(KeyCode.D)) { moveX += 1f; }
-        if (Input.GetKey(KeyCode.A)) { moveX -= 1f; }
+        if (Input.GetKey(KeyCode.W)) moveZ += 1;
+        if (Input.GetKey(KeyCode.S)) moveZ -= 1;
+        if (Input.GetKey(KeyCode.D)) moveX += 1;
+        if (Input.GetKey(KeyCode.A)) moveX -= 1;
 
-        Vector3 move = new Vector3(moveX, 0, moveZ).normalized * speed * Time.deltaTime;
+        Vector3 moveDirection = new Vector3(moveX, 0, moveZ).normalized;
+        Vector3 move = moveDirection * speed * Time.deltaTime;
         transform.position += move;
 
-        if (move != Vector3.zero)
+        if (moveDirection != Vector3.zero)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(move);
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
+
+        float rawSpeed = move.magnitude / Time.deltaTime;
+        animator.SetFloat("speed", rawSpeed);
+        animator.SetBool("isRunning", rawSpeed > 0.1f);
     }
 }
